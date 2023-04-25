be56a1652fd1241b1076bff792e8bcc55945a0ab
Abhishek <abhisheksing147@gmail.com>
2023-04-25 08:41:07 +0530

    change color through input

---
+++ DOM.js
@@ -0,0 +1,9 @@
+let a = document.getElementById("change").value
+    console.log(a)
+
+function action(){
+    let a = document.getElementById("change").value
+    console.log(a)
+    document.body.style.backgroundColor = a
+    
+}
